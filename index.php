ca77a669b7d06d46101c6f24b6c74d68c6d19e67
zalcat <51262837+zalcat@users.noreply.github.com>
2020-09-26 10:07:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>漏洞练习平台 ~~</title>
+    <title>Predators渗透测试演练系统</title>
     <link rel="stylesheet" href="layui/css/layui.css">
     <script src="layui/layui.js"></script>
     <script src="layui/layer/layer.js"></script>
